90e72b26ff9c5369bfb04771ef5b57f1ae59b643
Marco Massetti <63008676+mighty-mass@users.noreply.github.com>
2021-11-14 20:20:41 +0000

    Adding loading part and fix logic

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 //Create Table from file upload
 var file_upload = false;
 
+async function getFile(file) {
+  
+    try {
+        let res = await fetch(file);
+        return await res.text();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-function creatTableFromFile(){
+async function creatTableFromFile(){
   var file_upl = document.getElementById("tickers_list");
+  var tbl = document.getElementById("main_table_tickers");
+
+  data = await getFile("input_example.csv");  
+  lines = data.split("\n");
+  
+  tbl.innerHTML = "<tr><th>Ticker</th><th>Amount</th><th>Buy Price</th><th>Current Price</th><th>Gain</th></tr>";
+
+  for(let i = 0; i < lines.length; i++){
+    cols = lines[i].split(",");
+
+    let newRow = document.createElement('tr');
+    newRow.insertCell(0);
+    newRow.cells[0].innerHTML += "<input type=\"text\" value=\""+cols[0]+"\" />";
+    newRow.insertCell(1);
+    newRow.cells[1].innerHTML += "<input type=\"number\" step=\"0.01\" value=\""+parseFloat(cols[1])+"\" />";
+    newRow.insertCell(2);
+    newRow.cells[2].innerHTML += "<input type=\"number\" step=\"0.01\" value=\""+parseFloat(cols[2])+"\" />";
+    newRow.insertCell(3);
+    newRow.cells[3].innerHTML += "";
+    newRow.insertCell(4);
+    newRow.cells[4].innerHTML += "";
+    tbl.appendChild(newRow);
+  }
+  
+  file_upload = true;
+
 }
 
 //Table Add New Row
@@ -11,7 +46,7 @@ function addNewTableRow() {
   var tbl = document.getElementById("main_table_tickers");
   
   if(!file_upload){
-    document.getElementById("main_table_tickers").innerHTML = "<tr><th>Ticker</th><th>Amount</th><th>Buy Price</th><th>Current Price</th><th>Gain</th></tr>";
+    tbl.innerHTML = "<tr><th>Ticker</th><th>Amount</th><th>Buy Price</th><th>Current Price</th><th>Gain</th></tr>";
   }
   // Add a new row
   let newRow = document.createElement('tr');
@@ -22,9 +57,9 @@ function addNewTableRow() {
   newRow.insertCell(2);
   newRow.cells[2].innerHTML += "<input type=\"number\" step=\"0.01\" value=\"0\" />";
   newRow.insertCell(3);
-  newRow.cells[3].innerHTML += ""
+  newRow.cells[3].innerHTML += "";
   newRow.insertCell(4);
-  newRow.cells[4].innerHTML += ""
+  newRow.cells[4].innerHTML += "";
 
   tbl.appendChild(newRow);
 }
